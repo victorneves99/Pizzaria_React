8ebe16d48af65222e6a56f9d588c3e407f5cfbe0
victorneves99 <victorneves99@gmail.com>
2022-07-02 23:28:35 -0300

    iniciando projeto

---
+++ src/server.ts
@@ -0,0 +1,12 @@
+import { router } from "./routes";
+import express, { Request, Response, NextFunction } from "express";
+
+const app = express();
+
+app.use(express.json());
+
+app.use(router);
+
+app.listen(3333, () => {
+  console.log("Servidor online");
+});
